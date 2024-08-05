dedb9df43400e8c9ae06f030fb12f58b764b1449
Steve Fallet <steve.fallet@divtec.ch>
2024-08-05 17:13:35 +0200

    Ajout de commentaires

--- init.sql
+++ init.sql
@@ -1,3 +1,4 @@
+/* Donne les droits à l'utilisateur root et à l'utilisateur directus */
 ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'rootpassword';
 CREATE USER 'directus'@'%' IDENTIFIED WITH mysql_native_password BY 'directus';
 GRANT ALL PRIVILEGES ON *.* TO 'directus'@'%';
